7ab16cc330eaaad4d267859a7651a16242e12ab4
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2022-05-14 15:01:57 +0600

    linear search added

--- linear-search.cpp
+++ linear-search.cpp
@@ -1,11 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int linearSearch(){
-
+int linearSearch(int *linearArr, int key, int lenth){
+    for(int i=0; i<lenth; i++){
+        if(linearArr[i]==key){
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main(){
-	cout<<"Hello World"<<endl;
+	int linearArr[]={1,2,3,4,5,6,7};
+	int length = sizeof(linearArr)/sizeof(int);
+	int key;
+	cout<<"Enter the key: ";
+	cin>>key;
+	int index = linearSearch(linearArr, key, length);
+	if(index !=1){
+        cout<<key<<" Present at: " <<index<<endl;
+	}
+	else{
+        cout<<key <<" is not present!!"<<endl;
+	}
+
 	return 0;
 }
